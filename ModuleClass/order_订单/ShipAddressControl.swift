28aa5fd780b26a0b02a85d55f6a5139145b25d03
hailin <jjd510@163.com>
2018-03-28 20:04:27 +0800

    ship address

---
+++ ModuleClass/order_订单/ShipAddressControl.swift
@@ -0,0 +1,16 @@
+//
+//  ShipAddressControl.swift
+//  GreateSmart
+//
+//  Created by hailin on 2018/3/28.
+//  Copyright © 2018年 Jerod. All rights reserved.
+//
+
+import UIKit
+
+class ShipAddressControl: BaseControl {
+    
+    var view : UIView?
+    
+    
+}
